7327f6423e1bcd06ca910ceefb7c97b07b1c5881
Asabe Nasir <Zainabnasir952@gmail.com>
2026-08-05 16:07:57 +0100

    fix: update health endpoint and error handler

--- backend/app.js
+++ backend/app.js
@@ -1,13 +1,16 @@
 import express from "express";
-import { sendError, sendSuccess } from "./src/utils/response.js";
+import { sendSuccess } from "./src/utils/response.js";
 import { errorHandler, notFound } from "./src/middlewares/errorHandlers.js";
+
 const app = express();
+
 app.use(express.json());
 
-app.get("/", (req, res) => {
-	return sendSuccess(res, 200, { status: "ok" }, "server is running");
+app.get("/health", (req, res) => {
+  return sendSuccess(res, 200, "Server is running");
 });
 
 app.use(notFound);
 app.use(errorHandler);
+
 export default app;
